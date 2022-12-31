39ca66f7086f460d6ea0c14b421099e7bf08155f
Romulo Roriz <romulonmroriz@gmail.com>
2022-12-31 12:09:04 +0100

    Add error handler middleware and connectDB

--- server/index.js
+++ server/index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/db');
+const errorHandler = require('./middleware/errorMiddleware');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 require('colors');
+
+// Connect DB
+connectDB();
 
 // Express app
 const app = express();
 
 // Cors
 app.use(cors());
-
-// Connect DB
 
 // Middleware
 app.use(express.json()); // If the request has a body, parses and attaches to the request object
@@ -19,6 +22,9 @@ app.use((req, res, next) => {
 	console.log(req.path, req.method);
 	next();
 });
+
+// Error Handler
+app.use(errorHandler);
 
 // Listen for requests
 app.listen(port, () => {
