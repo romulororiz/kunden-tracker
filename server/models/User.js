84724f15e49f28ea9531094fa7bc3904e06fdc15
Romulo Roriz <romulonmroriz@gmail.com>
2023-01-03 18:12:07 +0100

    changes to model

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
 	googleId: { type: String },
 	email: { type: String },
-	profilePic: { type: String },
+	avatar: { type: String },
 	firstName: { type: String },
 	lastName: { type: String },
 	fullName: { type: String },
