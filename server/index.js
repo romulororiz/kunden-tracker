4b153f71e046b742bd047acc5fdb329538950eb8
Romulo Roriz <romulonmroriz@gmail.com>
2023-01-03 21:22:02 +0100

    change authentication method

--- server/index.js
+++ server/index.js
@@ -1,13 +1,9 @@
 const express = require('express');
 require('dotenv').config();
 require('colors');
-require('./auth');
-const session = require('express-session');
-const passport = require('passport');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/errorMiddleware');
-const protectRoute = require('./middleware/authMiddleware');
 const port = process.env.PORT || 5000;
 
 // Connect DB
@@ -15,19 +11,6 @@ connectDB();
 
 // Express app
 const app = express();
-
-// Express Session
-app.use(
-	session({
-		secret: require('crypto').randomBytes(64).toString('hex'),
-		resave: false,
-		saveUninitialized: true,
-	})
-);
-
-// Passport Initialization
-app.use(passport.initialize());
-app.use(passport.session());
 
 // Cors
 app.use(cors());
@@ -37,16 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
-// app.use('/api/clients');
-
-// Passport callback
-app.get(
-	'/google/callback',
-	passport.authenticate('google', {
-		successRedirect: `${process.env.CLIENT_URL}/dashboard`,
-		failureRedirect: '/api/users/auth/google',
-	})
-);
+app.use('/api/clients', require('./routes/clientRoutes'));
 
 // Error Handler
 app.use(errorHandler);
