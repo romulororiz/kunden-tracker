8385e5c88c43f57725d1309f509ed3907d45f107
Romulo Roriz <romulonmroriz@gmail.com>
2023-01-03 19:24:04 +0100

    user controller for handling logout from oauth2.0

---
+++ server/controllers/userController.js
@@ -0,0 +1,12 @@
+// Logout User
+const logout = (req, res, next) => {
+	req.logout(function (err) {
+		if (err) {
+			return next(err);
+		}
+		req.session.destroy();
+		res.redirect('/');
+	});
+};
+
+module.exports = logout;
