7fde93df1d8cf969aec739143b0c606cdd7313a4
Romulo Roriz <romulonmroriz@gmail.com>
2023-01-04 21:53:47 +0100

    Define get client by Id route

--- server/routes/clientRoutes.js
+++ server/routes/clientRoutes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const protectRoute = require('../middleware/authMiddleware');
-const { getClients, addClient } = require('../controllers/clientController');
+const {
+	getClients,
+	addClient,
+	getClient,
+} = require('../controllers/clientController');
 
 router.route('/').get(protectRoute, getClients).post(protectRoute, addClient);
+
+router.route('/:id').get(protectRoute, getClient);
 
 module.exports = router;
