fce47aa1d4cf07e03d1025c64fd1f2760365caf6
Romulo Roriz <romulonmroriz@gmail.com>
2023-01-05 21:09:49 +0100

    Protect dashboard route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,16 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from '@pages/Register';
 import Dashboard from '@components/Dashboard';
 import Login from '@pages/Login';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
 	return (
 		<>
 			<Router>
 				<Routes>
-					<Route path='/' exact element={<Dashboard />} />
+					<Route path='/' element={<PrivateRoute />}>
+						<Route path='/' element={<Dashboard />} />
+					</Route>
 					<Route path='/login' element={<Login />} />
 					<Route path='/register' element={<Register />} />
 				</Routes>
