14a43fd9aa591b41049429d76fd616eb8a5cb415
Romulo Roriz <romulonmroriz@gmail.com>
2023-01-05 19:22:46 +0100

    Setup scss and cleanup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './pages/Register';
 import Dashboard from './components/Dashboard';
-import './App.css';
 import Login from './pages/Login';
 
 function App() {
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ViteAliases } from 'vite-aliases';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), ViteAliases()],
 	server: {
 		port: 3000,
 		proxy: {
