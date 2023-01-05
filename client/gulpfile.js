01a4c1c237837f1ba80d5d4d1976166db6f44a33
Romulo Roriz <romulonmroriz@gmail.com>
2023-01-05 19:12:40 +0100

    Install gulp dependencies and setup gulpfile

---
+++ client/gulpfile.js
@@ -0,0 +1,22 @@
+const { src, dest, watch, series, task } = require('gulp');
+const autoPrefixer = require('gulp-autoprefixer');
+const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
+
+function buildStyles() {
+	return src('./src/styles/scss/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(
+			purgecss({
+				content: ['./src/**/*.jsx'],
+			})
+		)
+		.pipe(autoPrefixer())
+		.pipe(dest('./src/styles/css'));
+}
+
+function watchTask() {
+	watch(['./src/styles/scss/**/*.scss', './src/**/*.jsx'], buildStyles);
+}
+
+task('default', series(buildStyles, watchTask));
