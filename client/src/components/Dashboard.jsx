9162ebae30690fb44d0cf66866e9914d747f0448
Romulo Roriz <romulonmroriz@gmail.com>
2023-01-05 19:06:21 +0100

    uninstall passport and cryptojs

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,4 +1,18 @@
+import { useDispatch } from 'react-redux';
+import { logout } from '../features/auth/authSlice';
+
 const Dashboard = () => {
-	return <div>Dashboard</div>;
+	const dispatch = useDispatch();
+
+	const handleClick = () => {
+		dispatch(logout());
+	};
+
+	return (
+		<div>
+			Dashboard
+			<button onClick={handleClick}>Logout</button>
+		</div>
+	);
 };
 export default Dashboard;
