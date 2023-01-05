aa675fb6f5e2330d0e59279a573cd8dd6ca2fea8
Romulo Roriz <romulonmroriz@gmail.com>
2023-01-05 17:45:07 +0100

    Add sample data for clients

---
+++ server/data/clients.js
@@ -0,0 +1,132 @@
+export const clients = [
+	{
+		name: 'John Smith',
+		address: {
+			city: 'New York',
+			street: '123 Main St',
+			houseNumber: 'Apt 3',
+			postalCode: '10001',
+		},
+		workingHours: [
+			{
+				day: new Date('2022-01-03'),
+				startTime: new Date('2022-01-03T09:00:00'),
+				endTime: new Date('2022-01-03T17:00:00'),
+			},
+			{
+				day: new Date('2022-01-04'),
+				startTime: new Date('2022-01-04T09:00:00'),
+				endTime: new Date('2022-01-04T17:00:00'),
+			},
+			{
+				day: new Date('2022-01-05'),
+				startTime: new Date('2022-01-05T09:00:00'),
+				endTime: new Date('2022-01-05T17:00:00'),
+			},
+		],
+	},
+	{
+		name: 'Jane Doe',
+		address: {
+			city: 'Los Angeles',
+			street: '456 Market St',
+			houseNumber: '',
+			postalCode: '90001',
+		},
+		workingHours: [
+			{
+				day: new Date('2022-01-03'),
+				startTime: new Date('2022-01-03T10:00:00'),
+				endTime: new Date('2022-01-03T18:00:00'),
+			},
+			{
+				day: new Date('2022-01-04'),
+				startTime: new Date('2022-01-04T10:00:00'),
+				endTime: new Date('2022-01-04T18:00:00'),
+			},
+			{
+				day: new Date('2022-01-05'),
+				startTime: new Date('2022-01-05T10:00:00'),
+				endTime: new Date('2022-01-05T18:00:00'),
+			},
+		],
+	},
+	{
+		name: 'Bob Johnson',
+		address: {
+			city: 'Chicago',
+			street: '789 Elm St',
+			houseNumber: '',
+			postalCode: '60601',
+		},
+		workingHours: [
+			{
+				day: new Date('2022-01-03'),
+				startTime: new Date('2022-01-03T09:00:00'),
+				endTime: new Date('2022-01-03T17:00:00'),
+			},
+			{
+				day: new Date('2022-01-04'),
+				startTime: new Date('2022-01-04T09:00:00'),
+				endTime: new Date('2022-01-04T17:00:00'),
+			},
+			{
+				day: new Date('2022-01-05'),
+				startTime: new Date('2022-01-05T09:00:00'),
+				endTime: new Date('2022-01-05T17:00:00'),
+			},
+		],
+	},
+	{
+		name: 'Emily Williams',
+		address: {
+			city: 'Houston',
+			street: '321 Pine St',
+			houseNumber: '',
+			postalCode: '77001',
+		},
+		workingHours: [
+			{
+				day: new Date('2022-01-03'),
+				startTime: new Date('2022-01-03T09:00:00'),
+				endTime: new Date('2022-01-03T17:00:00'),
+			},
+			{
+				day: new Date('2022-01-04'),
+				startTime: new Date('2022-01-04T09:00:00'),
+				endTime: new Date('2022-01-04T17:00:00'),
+			},
+			{
+				day: new Date('2022-01-05'),
+				startTime: new Date('2022-01-05T09:00:00'),
+				endTime: new Date('2022-01-05T17:00:00'),
+			},
+		],
+	},
+	{
+		name: 'James Smith',
+		address: {
+			city: 'Philadelphia',
+			street: '654 Maple St',
+			houseNumber: '',
+			postalCode: '19101',
+		},
+		workingHours: [
+			{
+				day: new Date('2022-01-03'),
+				startTime: new Date('2022-01-03T09:00:00'),
+				endTime: new Date('2022-01-03T17:00:00'),
+			},
+			{
+				day: new Date('2022-01-04'),
+				startTime: new Date('2022-01-04T09:00:00'),
+				endTime: new Date('2022-01-04T17:00:00'),
+			},
+			{
+				day: new Date('2022-01-05'),
+				startTime: new Date('2022-01-05T09:00:00'),
+				endTime: new Date('2022-01-05T17:00:00'),
+			},
+		],
+	},
+];
