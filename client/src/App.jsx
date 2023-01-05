ce2f61b4a47d3e4b7a50e762e70a2bb3c5fcf4c4
Romulo Roriz <romulonmroriz@gmail.com>
2023-01-05 17:44:28 +0100

    define login route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './pages/Register';
 import Dashboard from './components/Dashboard';
 import './App.css';
+import Login from './pages/Login';
 
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 			<Router>
 				<Routes>
 					<Route path='/' exact element={<Dashboard />} />
+					<Route path='/login' element={<Login />} />
 					<Route path='/register' element={<Register />} />
 				</Routes>
 			</Router>
