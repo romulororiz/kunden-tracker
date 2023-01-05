b66278bbdd6e655583336f58f0f96c12ab605fb2
Romulo Roriz <romulonmroriz@gmail.com>
2023-01-05 18:06:21 +0100

    Define getAll clients slice and service

--- client/src/features/clients/clientsService.js
+++ client/src/features/clients/clientsService.js
@@ -10,7 +10,20 @@ export const addClient = async (clientData, token) => {
 		},
 	};
 
-	const { data } = await axios.post(API_URL, clientData, token);
+	const { data } = await axios.post(API_URL, clientData, config);
+
+	return data;
+};
+
+// Get all clients
+export const getClients = async token => {
+	const config = {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	};
+
+	const { data } = await axios.post(API_URL, config);
 
 	return data;
 };
--- client/src/features/clients/clientsSlice.js
+++ client/src/features/clients/clientsSlice.js
@@ -29,5 +29,23 @@ export const addClient = createAsyncThunk(
 	}
 );
 
+export const getClients = createAsyncThunk(
+	'clients/getAll',
+	async (_, thunkAPI) => {
+		try {
+			const token = thunkAPI.getState().auth.user.token;
+			return await clientsService.addClient(token);
+		} catch (error) {
+			const message =
+				(error.response &&
+					error.response.data &&
+					error.response.data.message) ||
+				error.message ||
+				error.toString();
+			return thunkAPI.rejectWithValue(message);
+		}
+	}
+);
+
 // export const { reset } = productSlice.actions;
 // export default productSlice.reducer;
