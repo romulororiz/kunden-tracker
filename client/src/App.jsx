a120e8bb2ee0e3cbf4ebf69229e5b78b18e36ba2
Romulo Roriz <romulonmroriz@gmail.com>
2023-01-06 19:23:07 +0100

    Add SidebarLayout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,26 +6,33 @@ import {
 } from 'react-router-dom';
 import Register from '@pages/Register';
 import Dashboard from '@components/Dashboard';
+import Clients from '@components/Clients';
 import Login from '@pages/Login';
 import PrivateRoute from './components/PrivateRoute';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import '@styles/scss/Layout.scss';
+import SidebarLayout from './components/SidebarLayout';
 
 const App = () => {
 	return (
 		<>
 			<Router>
 				<Routes>
-					<Route path='/' element={<PrivateRoute />}>
-						<Route
-							exact
-							path='/'
-							element={<Navigate to='/dashboard' />}
-						></Route>
-					</Route>
-
-					<Route path='/dashboard' element={<PrivateRoute />}>
-						<Route path='/dashboard' element={<Dashboard />} />
+					<Route element={<SidebarLayout />}>
+						<Route path='/' element={<PrivateRoute />}>
+							<Route
+								exact
+								path='/'
+								element={<Navigate to='/dashboard' />}
+							></Route>
+						</Route>
+						<Route path='/dashboard' element={<PrivateRoute />}>
+							<Route path='/dashboard' element={<Dashboard />} />
+						</Route>
+						<Route path='/clients' element={<PrivateRoute />}>
+							<Route path='/clients' element={<Clients />} />
+						</Route>
 					</Route>
 					<Route path='/login' element={<Login />} />
 					<Route path='/register' element={<Register />} />
