9536f0082b59ae2b532d3525880719a8b5261821
Romulo Roriz <romulonmroriz@gmail.com>
2023-01-06 16:24:19 +0100

    Define links and logo sidebar

--- client/src/components/Layout/Sidebar.jsx
+++ client/src/components/Layout/Sidebar.jsx
@@ -1,6 +1,37 @@
+import Logo from '@assets/svgs/logo.svg';
 import '@styles/scss/Sidebar.scss';
+import { TiHomeOutline } from 'react-icons/ti';
+import { HiOutlineUsers } from 'react-icons/hi';
+import { RiMarkupLine } from 'react-icons/ri';
 
 const Sidebar = () => {
-	return <aside className='sidebar'>Sidebar</aside>;
+	return (
+		<aside className='sidebar'>
+			<div className='sidebar__logo'>
+				<img src={Logo} alt='Logo' className='sidebar__logo-image' />
+			</div>
+			<div className='sidebar__section'>
+				<div className='sidebar__section-title'>Analytics</div>
+				<a href='#' className='sidebar__link'>
+					<TiHomeOutline className='sidebar__link-icon' />
+					Dashboard
+				</a>
+			</div>
+			<div className='sidebar__section'>
+				<div className='sidebar__section-title'>Content</div>
+				<a href='#' className='sidebar__link'>
+					<HiOutlineUsers className='sidebar__link-icon' />
+					Clients
+				</a>
+			</div>
+			<div className='sidebar__section'>
+				<div className='sidebar__section-title'>Customization</div>
+				<a href='#' className='sidebar__link'>
+					<RiMarkupLine className='sidebar__link-icon' />
+					Theme
+				</a>
+			</div>
+		</aside>
+	);
 };
 export default Sidebar;
