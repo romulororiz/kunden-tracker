96511084fc3f0c58fc1c28491e89b86b3c95d01b
Romulo Roriz <romulonmroriz@gmail.com>
2023-01-12 11:12:58 +0100

    Restructure table

--- client/src/components/Layout/ClientsTable.jsx
+++ client/src/components/Layout/ClientsTable.jsx
@@ -24,6 +24,65 @@ const ClientsTable = ({ columns, data, clients }) => {
 		usePagination
 	);
 
+	const tableData = (
+		<>
+			<table {...getTableProps()}>
+				<thead key='thead'>
+					{headerGroups.map((headerGroup, idx) => (
+						<tr key={`${idx}-trow`} {...headerGroup.getHeaderGroupProps()}>
+							{headerGroup.headers.map((column, idx) => (
+								<th key={`${idx}-thead`} {...column.getHeaderProps()}>
+									{column.render('Header')}
+								</th>
+							))}
+						</tr>
+					))}
+				</thead>
+				<tbody key='tbody' {...getTableBodyProps()}>
+					{page.map((row, idx) => {
+						prepareRow(row);
+						return (
+							<tr key={`${idx}-trow`} {...row.getRowProps()}>
+								{row.cells.map((cell, idx) => {
+									return (
+										<td key={`${idx}-tdata`} {...cell.getCellProps()}>
+											{cell.render('Cell')}
+										</td>
+									);
+								})}
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+			<div className='table__pagination-wrapper'>
+				<span className='table__clients-count'>
+					Showing {page.length} of {clients.length} clients
+				</span>
+				<div className='pagination'>
+					<button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+						{'<<'}
+					</button>{' '}
+					<button onClick={() => previousPage()} disabled={!canPreviousPage}>
+						{'<'}
+					</button>{' '}
+					<button onClick={() => nextPage()} disabled={!canNextPage}>
+						{'>'}
+					</button>{' '}
+					<button
+						onClick={() => gotoPage(pageCount - 1)}
+						disabled={!canNextPage}
+					>
+						{'>>'}
+					</button>{' '}
+				</div>
+				<span className='table__page-count'>
+					Page {pageIndex + 1} of {pageOptions.length}
+				</span>
+			</div>
+		</>
+	);
+
 	return (
 		<>
 			{!data.length ? (
@@ -31,63 +90,7 @@ const ClientsTable = ({ columns, data, clients }) => {
 					<p>No clients found</p>
 				</div>
 			) : (
-				<>
-					<table {...getTableProps()}>
-						<thead>
-							{headerGroups.map((headerGroup, idx) => (
-								<tr {...headerGroup.getHeaderGroupProps()}>
-									{headerGroup.headers.map((column, idx) => (
-										<th {...column.getHeaderProps()}>
-											{column.render('Header')}
-										</th>
-									))}
-								</tr>
-							))}
-						</thead>
-						<tbody {...getTableBodyProps()}>
-							{page.map((row, idx) => {
-								prepareRow(row);
-								return (
-									<tr {...row.getRowProps()}>
-										{row.cells.map((cell, idx) => {
-											return (
-												<td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-											);
-										})}
-									</tr>
-								);
-							})}
-						</tbody>
-					</table>
-					<div className='table__pagination-wrapper'>
-						<span className='table__clients-count'>
-							Showing {page.length} of {clients.length} clients
-						</span>
-						<div className='pagination'>
-							<button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-								{'<<'}
-							</button>{' '}
-							<button
-								onClick={() => previousPage()}
-								disabled={!canPreviousPage}
-							>
-								{'<'}
-							</button>{' '}
-							<button onClick={() => nextPage()} disabled={!canNextPage}>
-								{'>'}
-							</button>{' '}
-							<button
-								onClick={() => gotoPage(pageCount - 1)}
-								disabled={!canNextPage}
-							>
-								{'>>'}
-							</button>{' '}
-						</div>
-						<span className='table__page-count'>
-							Page {pageIndex + 1} of {pageOptions.length}
-						</span>
-					</div>
-				</>
+				<>{tableData}</>
 			)}
 		</>
 	);
