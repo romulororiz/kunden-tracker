36b88b7f8d0349cc136394e5423016456684d7e7
Romulo Roriz <romulonmroriz@gmail.com>
2023-01-12 11:14:30 +0100

    Remove notification context and onClickOutside hook

--- client/src/hooks/useNotificationContext.jsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react';
-import { NotificationContext } from '@context/NotificationContext';
-
-export const useNotificationContext = () => {
-	const context = useContext(NotificationContext);
-
-	return context;
-};
