8148094ee3a3875cf4a8f9eca854cd7255b364a2
Romulo Roriz <romulonmroriz@gmail.com>
2023-01-15 16:28:15 +0100

    fix path build

--- server/index.js
+++ server/index.js
@@ -29,10 +29,10 @@ app.use(errorHandler);
 if (process.env.NODE_ENV === 'production') {
 	// Serve frontend
 	// Set build folder as static
-	app.use(express.static(path.join(__dirname, '../client/build')));
+	app.use(express.static(path.join(__dirname, '../client/dist')));
 
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
+		res.sendFile(path.resolve(__dirname, '../client/dist/index.html'));
 	});
 }
 
